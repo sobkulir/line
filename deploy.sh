d58137936faff51e0aebefe855555a3eb5c9e44b
Roman Sobkuliak <r.sobkuliak@gmail.com>
2024-02-11 15:45:44 +0100

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 
 git checkout --quiet main
+
+set +e
 something_changed=$(git diff-index --exit-code --ignore-submodules HEAD)
+set -e
+
 if [ -n "$something_changed" ]
 then
     echo >&2 "main has some changes, I cannot deploy."
@@ -9,7 +14,17 @@ then
 fi
 
 commit_id=$(git show-ref --head | head -c6)
-git switch -c gh-pages
+
+set +e
+gh_pages_exist=$(git show-ref --verify --quiet refs/heads/gh-pages)
+set -e
+
+if $gh_pages_exist; then
+    git checkout gh-pages
+else
+    git checkout -b gh-pages
+fi
+
 git checkout main -- src
 
 version_name=${commit_id}
